728b16940b2f31d1fab6aaca4f262dc1b1459f53
Nelsonjk193 <114142312+Nelsonjk193@users.noreply.github.com>
2022-11-24 14:24:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 9000
 
 app.get('/', (req, res) => {
-  res.send('Hello World! - welcome to icu program - nelson jk12')
+  res.send('Hello World! - welcome to icu program - nelson jk123')
 })
 
 app.listen(port, () => {
